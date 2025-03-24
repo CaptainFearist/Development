5ce0979fd2ed7760a3da11de9717fa1a31a55869
CaptainFearist <igorgolenisev1317@gmail.com>
2025-03-24 18:47:57 +0300

    Field validation logic updated

--- js/script.js
+++ js/script.js
@@ -1,31 +1,62 @@
 function checkFields() {
     let firstUserName = document.querySelector(`#firstName`).value;
+    let firstNameError = document.querySelector(`#firstNameError`);
+    firstNameError.style.display = `none`;
         if(/[-_+=.,/'";:~|]/.test(firstUserName)) {
-            alert(`Такого в имени не может быть!`);
+            firstNameError.textContent = `Такого в имени не может быть!`;
+            firstNameError.style.display = `block`;
+            setTimeout(function(){
+                firstNameError.style.display = `none`;
+            }, 3000);
             return;
         }
         if(firstUserName.length < 3) {
-            alert(`Имя не может быть настолько коротким!`);
+            firstNameError.textContent = `Имя не может быть настолько коротким!`;
+            firstNameError.style.display = `block`;
+            setTimeout(function(){
+                firstNameError.style.display = `none`;
+            }, 3000);
             return;
         }
     let lastUserName = document.querySelector(`#lastName`).value;
+    let lastNameError = document.querySelector(`#lastNameError`);
+    lastNameError.style.display = `none`;
         if(/[-_+=.,/'";:~|]/.test(lastUserName)) {
-            alert(`Такого в фамилии быть не может!`);
+            lastNameError.textContent = `Такого в фамилии быть не может!`;
+            lastNameError.style.display = `block`;
+            setTimeout(function(){
+                lastNameError.style.display = `none`;
+            }, 3000);
             return;
         }
         if(lastUserName.length < 2) {
-            alert(`Фамилия не может быть одной буквой!`);
+            lastNameError.textContent = `Фамилия не может быть одной буквой!`;
+            lastNameError.style.display = `block`;
+            setTimeout(function(){
+                lastNameError.style.display = `none`;
+            }, 3000);
             return;
         }
     let mailAddress = document.querySelector(`#emailAddress`).value;
+    let mailError = document.querySelector(`#mailError`);
+    mailError.style.display = `none`;
         if(!mailAddress.includes(`@`)) {
-            alert(`Нет символа @`);
+            mailError.textContent = `Нет символа @`;
+            mailError.style.display = `block`;
+            setTimeout(function(){
+                mailError.style.display = `none`;
+            }, 3000);
             return;
         }
         if(!mailAddress.includes(`.`)) {
-            alert(`Нет символа .`);
+            mailError.textContent = `Нет символа .`;
+            mailError.style.display = `block`;
+            setTimeout(function(){
+                mailError.style.display = `none`;
+            }, 3000);
             return;
         }
 
     alert(`Мы получили ваще сообщение`);
 }
+
